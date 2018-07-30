5aa09c0ca01e7d4f32efef4e936120b6490eddf8
philg <phil.graetz@outlook.com>
2018-07-30 16:48:53 -0500

    added routes

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+path = require("path");
+
 
 // create our port (two definitions depending on site being local or hosted)
 var PORT = process.env.PORT || 8080;
 var app = express();
+
 
 // Serve content at routes from '/static'  for the app from the "public" directory in the application directory.
 app.use('/static', express.static('public'));
@@ -13,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/views/public')));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -22,6 +26,8 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 // the Controllers will handle the requests sent from the view
+require("./routes/html-routes")(app);
+
 
 // TODO: CREATE CONTROLLERS AND IMPORT THEM HERE
 //var routes = require("");
