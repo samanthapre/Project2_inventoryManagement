7f3cb868bbe5c6c021b10ebd8d1030cce2adac92
jeherringer <j.e.herringer@gmail.com>
2018-08-01 11:20:05 -0500

    button changes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,10 +1,11 @@
 var check = function() {
-    if (document.getElementById('userPassSignup').value ==
-      document.getElementById('userPassConfirm').value) {
+    if (document.getElementById('userPassSignup').value == document.getElementById('userPassConfirm').value) {
       document.getElementById('message').style.color = 'green';
       document.getElementById('message').innerHTML = 'Passwords match';
+      $(`#signupSubmit`).attr(`disabled`, false);
     } else {
       document.getElementById('message').style.color = 'red';
       document.getElementById('message').innerHTML = 'Passwords do not match';
+      $(`#signupSubmit`).attr(`disabled`, true);
     }
   }
