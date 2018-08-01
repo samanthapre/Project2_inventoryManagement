09afef1315e11cfe564b3e6cf089c33170f31b67
jeherringer <j.e.herringer@gmail.com>
2018-08-01 13:03:41 -0500

    back and forth achieved

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
     // GET /  Returns Landing page
     app.get("/", function(req, res) {
         console.log("Getting root");
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+        res.sendFile(path.join(__dirname, "/static/index.html"));
     });
 
 };
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,11 +1,33 @@
 var check = function() {
-    if (document.getElementById('userPassSignup').value == document.getElementById('userPassConfirm').value) {
-      document.getElementById('message').style.color = 'green';
-      document.getElementById('message').innerHTML = 'Passwords match';
-      $(`#signupSubmit`).attr(`disabled`, false);
-    } else {
-      document.getElementById('message').style.color = 'red';
-      document.getElementById('message').innerHTML = 'Passwords do not match';
-      $(`#signupSubmit`).attr(`disabled`, true);
-    }
+  if ($('#userPassSignup').val() == $('#userPassConfirm').val()) {
+    $('#message').css("color" , 'green');
+    $('#message').text ('Passwords match');
+    $(`#signupSubmit`).attr(`disabled`, false);
+  } else {
+    $('#message').css("color" , 'red');
+    $('#message').text('Passwords do not match') ;
+    $(`#signupSubmit`).attr(`disabled`, true);
   }
+}
+
+$(document).ready(function() {
+  $(`#signupSubmit`).on("click", function(event) {
+    event.preventDefault();
+
+    console.log("Submit User");
+    // GET an array of all users
+    $.get("/api/users", function(res){
+      console.log(res);
+      //loop through the users array
+      for (var i in res) {
+        // check if username being entered
+        if (res.login == $(`#usernameSignup`).value) {
+          $(`#usernameMessage`).html("Username already taken!");
+        }
+        else {
+          console.log("User created!")
+        }
+      }
+    })
+  }); 
+})
