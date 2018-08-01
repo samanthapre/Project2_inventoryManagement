2f0fffd994cd96de445853016ca742fffc9bfd60
Nic_Logan <nicholaslogan255@gmail.com>
2018-07-31 22:30:29 -0500

    Fixed identical id names in index.html

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,10 +1,10 @@
-var check = function() {
-    if (document.getElementById('userPassSignup').value ==
-      document.getElementById('userPassConfirm').value) {
-      document.getElementById('message').style.color = 'green';
-      document.getElementById('message').innerHTML = 'Passwords match';
-    } else {
-      document.getElementById('message').style.color = 'red';
-      document.getElementById('message').innerHTML = 'Passwords do not match';
-    }
+var check = function () {
+  if (document.getElementById('userPassSignup').value ==
+    document.getElementById('userPassConfirm').value) {
+    document.getElementById('message').style.color = 'green';
+    document.getElementById('message').innerHTML = 'Passwords match';
+  } else {
+    document.getElementById('message').style.color = 'red';
+    document.getElementById('message').innerHTML = 'Passwords do not match';
   }
+}
