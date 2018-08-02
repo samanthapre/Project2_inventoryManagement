4a8798ad1ce86be8d68dadb33943b2f1f36bb690
Nic_Logan <nicholaslogan255@gmail.com>
2018-08-02 10:54:00 -0500

    Fixed creating new user

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -9,51 +9,83 @@ let db = require('../models');
 // POST    /api/product      JSON   Newly added product
 // DELETE  /api/product/:id  id     None
 
-module.exports = function(app) {
+module.exports = function (app) {
 
     // GET /api/users  - Returns list of all Users
-    app.get("/api/users", function(req, res) {
+    app.get("/api/users", function (req, res) {
         console.log("GET /api/users");
         db.User.findAll()
-            .then(function(dbUser) {
+            .then(function (dbUser) {
                 res.json(dbUser);
             });
     });
 
     // POST /api/user  - Adds a new User
-    app.post("/api/user", function(req, res) {
-        console.log("user created");
-        console.log(req.body);
-        db.User.create(req.body)
-            .then(function(dbUser) {
-                res.json(dbUser);
+    app.post("/api/user", function (req, res) {
+        console.log("attempting to create user");
+        
+
+        db.User.findAll()
+            .then(function (userArr) {
+                
+                console.log("new user?: ",req.body);
+                //console.log("req.body.login: ",req.body.login);
+
+                // flag to check if username (login) has already been taken
+                var nameTaken = false;
+
+                // for every entry in user table..
+                for (var i in userArr) {
+
+                   // console.log("Arr{i}",userArr[i]);
+                    console.log("Current users: ",userArr[i].login);
+                    
+                    //check if username being entered is already in database..
+                    if (req.body.login == userArr[i].login) {
+                        nameTaken = true; // .. then the name is taken
+                    }
+                }
+
+                // if username is open..
+                if (!nameTaken) {
+
+                    // ...add the user
+                    db.User.create(req.body)
+                        .then(function (dbUser) {
+                            res.json("true"); // 'return' true to confirm addition of user
+                        });
+
+                }
+                else{ //  else, 'return' error false
+                    res.json("false");
+                }
             });
     });
 
     // GET /api/products  - Returns list of all Products
-    app.get("/api/products", function(req, res) {
+    app.get("/api/products", function (req, res) {
         console.log("GET /api/products");
         db.Product.findAll()
-            .then(function(dbProduct) {
+            .then(function (dbProduct) {
                 res.json(dbProduct);
             });
     });
 
     // POST /api/product  - Adds a new Product
-    app.post("/api/product", function(req, res) {
+    app.post("/api/product", function (req, res) {
         db.Product.create(req.body)
-            .then(function(dbProduct) {
+            .then(function (dbProduct) {
                 res.json(dbProduct);
             });
     });
 
     // DELETE /api/product/:id  -- Deletes a Product
-    app.delete("/api/product/:id", function(req, res) {
+    app.delete("/api/product/:id", function (req, res) {
         console.log(req.params);
         console.log("Deleting Product id: " + req.params.id);
         db.Product.destroy({
-            where: {id: req.params.id}
-        }).then(function(dbProduct) {
+            where: { id: req.params.id }
+        }).then(function (dbProduct) {
             res.json(dbProduct);
         });
     });
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -11,73 +11,135 @@ var check = function () {
 }
 
 $(document).ready(function () {
+
+  // create new user button pressed
   $(`#signupSubmit`).on("click", function (event) {
     event.preventDefault();
-    console.log("signupSubmit pressed");
+    //console.log("signupSubmit pressed");
 
+    // check for space character
     if ($(`#usernameSignup`).val().includes(" ")) {
       $(`#usernameMessage`).css("color", "red");
       $(`#usernameMessage`).html("Username cannot include spaces");
     }
-    
-    else {
-      // GET an array of all users
-      $.get("/api/users", function (res) {
-        console.log(res);
+    else { // if username has no whitespace
 
-        var nameTaken = false;
+      var newUser = $(`#usernameSignup`).val();
+      var newUsername = $(`#usernameSignup`).val();
 
-        //loop through the users array
-        for (var i in res) {
-          // check if username being entered
-          if (res.login == $(`#usernameSignup`).val()) {
-            nameTaken = true;
+
+      // GERNERATE SALT ========================
+      function dec2hex(dec) {
+        return ('0' + dec.toString(16)).substr(-2)
+      }
+
+      // generateId :: Integer -> String
+      function generateId(len) {
+        var arr = new Uint8Array((len || 40) / 2)
+        window.crypto.getRandomValues(arr)
+        return Array.from(arr, dec2hex).join('')
+      }
+
+      // create a new random 10 digit salt for the new user
+      var curSalt = generateId(10);
+      // ===========================================
+
+      var curPass = $(`#userPassSignup`).val() /// get the user password
+      var curHash = sha256(curPass + curSalt); // encrypt the password and salt
+
+      var newUser = {
+        name: $(`#nameSignup`).val(),
+        login: $(`#usernameSignup`).val(),
+        hash: curHash,
+        salt: curSalt
+      }
+
+      // attempt to add new user to the database 
+      // if username is unique, server will add to database
+      // otherwise, show error to user
+      $.post("/api/user", newUser)
+        .then(function (res) {
+          console.log(res);
+
+          if (res == "true") {
+            alert("User created!");
+            window.location.replace("/products");
+          }
+          else {
+            $(`#usernameMessage`).css("color", "red");
             $(`#usernameMessage`).html("Username already taken!");
           }
 
-        }
 
-        if (!nameTaken) {
-          console.log("name not taken")
+        });
 
-          //  GERNERATE SALT ========================
-          function dec2hex(dec) {
-            return ('0' + dec.toString(16)).substr(-2)
-          }
 
-          // generateId :: Integer -> String
-          function generateId(len) {
-            var arr = new Uint8Array((len || 40) / 2)
-            window.crypto.getRandomValues(arr)
-            return Array.from(arr, dec2hex).join('')
-          }
+      // ======OLD BUT SHOULD WORK===========
+      // GET an array of all users
+      //   $.get("/api/users", function (res) {
+      //     console.log(res); // log users
 
-          var curSalt = generateId(10);
-          // ===========================================
+      //     var nameTaken = false; // flag for unique username
 
-          var curPass = $(`#userPassSignup`).val()
-          var curHash = sha256(curPass + curSalt);
+      //     // Check if new username/login-name is unique to the database
+      //     for (var i in res) {
 
-          console.log(curHash);
-          console.log(curSalt);
-          console.log($(`#usernameSignup`).val());
+      //       // check if username being entered is already in database..
+      //       if (res.login == $(`#usernameSignup`).val()) { 
+      //         nameTaken = true; // .. then the name is taken
+      //         $(`#usernameMessage`).html("Username already taken!");
+      //       }
 
-          var newUser = {
-            name: $(`#nameSignup`).val(),
-            login: $(`#usernameSignup`).val(),
-            hash: curHash,
-            salt: curSalt
-          }
+      //     }
 
-          $.post("/api/user", newUser)
-            .then(function (res) {
-              console.log(res);
-            });
+      //     if (!nameTaken) { // if name was not taken, create new user
+      //       //console.log("name not taken")
 
-        } // end if
-      }); // get request
-      // redirect to products page
-      window.location.replace("/products")
+      //       //  GERNERATE SALT ========================
+      //       function dec2hex(dec) {
+      //         return ('0' + dec.toString(16)).substr(-2)
+      //       }
+
+      //       // generateId :: Integer -> String
+      //       function generateId(len) {
+      //         var arr = new Uint8Array((len || 40) / 2)
+      //         window.crypto.getRandomValues(arr)
+      //         return Array.from(arr, dec2hex).join('')
+      //       }
+
+      //       // create a new random 10 digit salt for the new user
+      //       var curSalt = generateId(10);
+      //       // ===========================================
+
+      //       var curPass = $(`#userPassSignup`).val() /// get the user password
+      //       var curHash = sha256(curPass + curSalt); // encrypt the password and salt
+
+      //       //console.log(curHash);
+      //       //console.log(curSalt);
+      //       //console.log($(`#usernameSignup`).val());
+
+      //       // create new object for the new user
+      //       var newUser = {
+      //         name: $(`#nameSignup`).val(),
+      //         login: $(`#usernameSignup`).val(),
+      //         hash: curHash,
+      //         salt: curSalt
+      //       }
+
+      //       // add new user to the database 
+      //       $.post("/api/user", newUser)
+      //         .then(function (res) {
+      //           console.log(res);
+      //         });
+
+      //     } // end if
+      //   }); // get request
+      //   // after creating new user, 
+      //   // redirect to products page
+      //   //window.location.replace("/products");
+      //   alert("User Created");
+
+
     }
   }); // signupButton pressed
 }); // on page load
